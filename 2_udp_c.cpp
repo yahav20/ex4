e9c9f9479363e679e53269421e6b26ef4ae79536
yahav20 <yahavezer256@gmail.com>
2024-11-25 22:25:02 +0200

    cpp udp server

--- 2_udp_c.cpp
+++ 2_udp_c.cpp
@@ -11,23 +11,27 @@ using namespace std;
 int main() {
 
     const char* ip_address = "127.0.0.1";
-    const int port_no = 5555;
-
+    //changed port
+    const int port_no = 1255;
+    // create udp socket using IPv4
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    // checking if socket creating succeed
     if (sock < 0) {
         perror("error creating socket");
     }
 
     struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(ip_address);
-    sin.sin_port = htons(port_no);
-
+      // clearing all memory to get rid of junk data
+      memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET; //ipv4
+    sin.sin_addr.s_addr = inet_addr(ip_address); //bind to server ip
+    sin.sin_port = htons(port_no); //converting port number to network bytes
+    //change size of the message
     char data[] = "hello";
     int data_len = sizeof(data);
-
+    // try to send message to server
     int sent_bytes = sendto(sock, data, data_len, 0, (struct sockaddr *) &sin, sizeof(sin));
+    //check if socket send succeed
     if (sent_bytes < 0) {
         perror("error writing to socket");
     }
@@ -35,13 +39,15 @@ int main() {
     struct sockaddr_in from;
     unsigned int from_len = sizeof(struct sockaddr_in);
     char buffer[4096];
+    // Receive data from the server
+    // write data to buffer array and save sender info using sturct sockadd_in
     int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
     if (bytes < 0) {
         perror("error reading from socket");
     }
 
     cout << "The server sent: " << buffer << endl;
-
+    //close socker
     close(sock);
 
     return 0;
--- 2_udp_s.cpp
+++ 2_udp_s.cpp
@@ -1,3 +1,4 @@
+// importing all neccercy socket libaries
 #include <iostream>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -9,29 +10,33 @@
 using namespace std;
 
 int main() {
-
-    const int server_port = 5555;
-
+    //changed port
+    const int server_port = 1255;
+    // create udp socket using IPv4
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    // checking if socket creating succeed
     if (sock < 0) {
         perror("error creating socket");
     }
 
     struct sockaddr_in sin;
-
+    // clearing all memory to get rid of junk data
     memset(&sin, 0, sizeof(sin));
-
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(server_port);
-
+    // defining server properties
+    sin.sin_family = AF_INET; //ipv4
+    sin.sin_addr.s_addr = INADDR_ANY; //bind to any ip
+    sin.sin_port = htons(server_port); //converting port number to network bytes
+    // bind the socket in check if socket bind succeed
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("error binding to socket");
     }
 
     struct sockaddr_in from;
     unsigned int from_len = sizeof(struct sockaddr_in);
-    char buffer[4096];
+    //change size of the message
+    char buffer[2048];
+    // Receive data from the client
+    // write data to buffer array and save sender info using sturct sockadd_in
     int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
     if (bytes < 0) {
         perror("error reading from socket");
@@ -39,11 +44,16 @@ int main() {
 
     cout << "The client sent: " << buffer << endl;
 
+    // Convert the message to uppercase
+    for (int i = 0; i < bytes; i++) {
+        buffer[i] = toupper(buffer[i]); // Convert each character to uppercase
+    }
+    // send echo of the data back to client
     int sent_bytes = sendto(sock, buffer, bytes, 0, (struct sockaddr *) &from, sizeof(from));
     if (sent_bytes < 0) {
         perror("error writing to socket");
     }
-
+    // Close the socket
     close(sock);
 
     return 0;
